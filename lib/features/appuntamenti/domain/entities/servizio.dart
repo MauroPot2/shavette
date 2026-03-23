fc00cd87bd2cafa2e25975d55720ae4f572d0e5f
MauroPot <mauroleonardo.potestio@gmail.com>
2026-03-23 18:40:54 +0100

    feat(domain): create entities Servizio & Appuntamento

---
+++ lib/features/appuntamenti/domain/entities/servizio.dart
@@ -0,0 +1,13 @@
+class Servizio {
+  final String id;
+  final String nome;
+  final double prezzo;
+  final int durataMinuti;
+
+  const Servizio({
+    required this.id,
+    required this.nome,
+    required this.prezzo,
+    required this.durataMinuti,
+  });
+}
