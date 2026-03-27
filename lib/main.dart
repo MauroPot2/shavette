b3dfba6fa1b010a0302ae757c11109b71017795a
MauroPot <mauroleonardo.potestio@gmail.com>
2026-03-27 13:17:34 +0100

    feat(routing): inizializzato go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shavette/core/router/app_router.dart';
 import 'package:shavette/core/theme/app_theme.dart';
-import 'package:shavette/features/servizi/presentation/screens/menu_servizi_screen.dart'; 
 
 void main() {
-  runApp(const ProviderScope(
-    child: ShavetteApp()
-    ),);
+  runApp(const ProviderScope(child: ShavetteApp()));
 }
 
+///classe main dell'app
 class ShavetteApp extends StatelessWidget {
+  ///costruttore della classe main.
   const ShavetteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const coloreBrandBarbiere = Color(0xFF1E3A8A);
-
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Shavette',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.getTheme(brandColor: coloreBrandBarbiere),
-      home: const MenuServiziScreen(), /// Punto di ingresso App.-
+      theme: AppTheme.getTheme(brandColor: const Color(0xFF1E3A8A)),
+
+      /// Usiamo il router qui.
+      routerConfig: appRouter,
     );
   }
 }
